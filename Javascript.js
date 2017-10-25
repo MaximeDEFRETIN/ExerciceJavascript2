46e91c1e33a732d238c1b57c9334c95e3465e32a
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-10-25 08:30:53 +0200

    Commentaire

--- Javascript.js
+++ Javascript.js
@@ -2,4 +2,5 @@ var nom = "Nom : Doe";
 var prenom = "Prenom : John";
 var ville = "Ville : New York";
 
+//'\n' permet de faire un saut de ligne en javascript
 alert(nom + '\n' + prenom + '\n' + ville);
